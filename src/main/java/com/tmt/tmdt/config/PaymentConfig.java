71bdd067f91bbaa17a48638a0cdd02362cc3fdf4
Tuan Minh <minhtuanto179@gmail.com>
2022-05-14 18:16:45 +0700

    fix home page productbycategory page

--- src/main/java/com/tmt/tmdt/config/PaymentConfig.java
+++ src/main/java/com/tmt/tmdt/config/PaymentConfig.java
@@ -2,7 +2,11 @@ package com.tmt.tmdt.config;
 
 public class PaymentConfig {
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-    public static String vnp_Returnurl = "https://tmt-ecommerce.herokuapp.com/payment/returnRs";
+    //on production
+//    public static String vnp_Returnurl = "https://tmt-ecommerce.herokuapp.com/payment/vnpay-checkout-result";
+
+
+    public static String vnp_Returnurl = "http://localhost:5000/payment/vnpay-checkout-result";
     public static String vnp_TmnCode = "UFWSKFT5";
     public static String vnp_HashSecret = "NNNHHXBFYQZTYWLLTYIKMZZHRTOBYQFM";
     public static String vnp_apiUrl = "http://sandbox.vnpayment.vn/merchant_webapi/merchant.html";
--- src/main/java/com/tmt/tmdt/controller/home/PaymentController.java
+++ src/main/java/com/tmt/tmdt/controller/home/PaymentController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,13 +20,12 @@ import java.util.*;
 @Slf4j
 @Controller
 public class PaymentController {
-    @PostMapping("/payment/redirectToBankForm")
+    @PostMapping("/payment/redirect-vnpay-checkout")
     @ResponseBody
-    public String createQueryUrl(HttpServletRequest req) throws UnsupportedEncodingException {
+    public String redirectVnPayCheckout(HttpServletRequest req) throws UnsupportedEncodingException {
         String vnp_Version = "2.1.0";
         String vnp_Command = "pay";
         String vnp_OrderInfo = "thong tin don hang";
-        String orderType = "other";
         String vnp_TxnRef = PaymentHelper.getRandomNumber(8);
         String vnp_IpAddr = PaymentHelper.getIpAddress(req);
         String vnp_TmnCode = PaymentConfig.vnp_TmnCode;
@@ -37,10 +37,7 @@ public class PaymentController {
         vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
         vnp_Params.put("vnp_Amount", String.valueOf(amount));
         vnp_Params.put("vnp_CurrCode", "VND");
-        String bank_code = req.getParameter("bankcode");
-        if (bank_code != null && !bank_code.isEmpty()) {
-            vnp_Params.put("vnp_BankCode", bank_code);
-        }
+        vnp_Params.put("vnp_BankCode", "NCB");
         vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
         vnp_Params.put("vnp_OrderInfo", vnp_OrderInfo);
         vnp_Params.put("vnp_OrderType", "topup");
@@ -103,14 +100,13 @@ public class PaymentController {
         return "home/payment/showPmBtn";
     }
 
-////        return "redirect:https://sandbox.vnpayment.vn/paymentv2/vpcpay.html?vnp_Amount=1806000&vnp_Command=pay&vnp_CreateDate=20210801153333&vnp_CurrCode=VND&vnp_IpAddr=127.0.0.1&vnp_Locale=vn&vnp_OrderInfo=Thanh+toan+don+hang+%3A5&vnp_OrderType=other&vnp_ReturnUrl=https%3A%2F%2Fdomainmerchant.vn%2FReturnUrl&vnp_TmnCode=DEMOV210&vnp_TxnRef=5&vnp_Version=2.1.0&vnp_SecureHash=3e0d61a0c0534b2e36680b3f7277743e8784cc4e1d68fa7d276e79c23be7d6318d338b477910a27992f5057bb1582bd44bd82ae8009ffaf6d141219218625c42";
-//
-//    }
 
-
-    @GetMapping("/payment/returnRS")
-    public String returnPage(Model model,
-                             HttpServletRequest request) {
+    @GetMapping("/payment/vnpay-checkout-result")
+    public String showVnpayCheckoutResult(Model model, @RequestParam Map<String, String> fields) {
+        System.out.println("_____________________________________++++_____________________________________");
+        for (String key : fields.keySet()) {
+            System.out.println(key + " : " + fields.get(key));
+        }
 
         return "home/payment/rspm";
     }
